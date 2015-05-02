18a3458b1526838852e86a92f20bd3d0add5290b
visualserge <sergio.delossantosjr@hotmail.com>
2015-05-02 13:25:51 +0800

    Add new Result class for LogIn validation.

---
+++ WebMain.Core/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebMain.Core
+{
+    public class Result
+    {
+        public int code { get; set; }
+
+        public bool issuccess { get; set; }
+    }
+}
